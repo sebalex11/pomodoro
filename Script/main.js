9572e3cb1295ad0875bc44844280e6aec19f5361
Sebastian Matthews <admiraloven@gmail.com>
2017-03-11 20:36:36 -0500

    added the functionality of the plus/minus buttons

--- Script/main.js
+++ Script/main.js
@@ -1,4 +1,23 @@
 $(function(){
 	const minute = 60;
-	
+
+	var settingChange = function(){
+		var $pressed = $(this);
+		var sign = $pressed.html();
+		var setting = $pressed.parent();
+		var section = $pressed.attr("data-div");
+		var $span = $("#"+section+" > span");
+		var tempNum = parseInt($span.html()) 
+
+		if($pressed.attr("data-sign") == "-" && tempNum > 1){
+			tempNum--;
+			$span.html(tempNum);
+		} else if($pressed.attr("data-sign") == "+" && tempNum >= 1){
+			tempNum++;
+			$span.html(tempNum);
+		}
+
+	}
+
+	$("button").on("click", settingChange);
 })
