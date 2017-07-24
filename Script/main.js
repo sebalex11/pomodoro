e2b5224ab168d32cfe353da27bc1a40f8e2b4aea
Sebastian Matthews <admiraloven@gmail.com>
2017-07-23 21:32:43 -0400

    added javascript and functions

--- Script/main.js
+++ Script/main.js
@@ -1,6 +1,9 @@
 $(function(){
 	const minute = 60;
+	var timerOn = false;
 
+
+//this gives functionality to the buttons for the clock settings
 	var settingChange = function(){
 		var $pressed = $(this);
 		var sign = $pressed.html();
@@ -8,6 +11,7 @@ $(function(){
 		var section = $pressed.attr("data-div");
 		var $span = $("#"+section+" > span");
 		var tempNum = parseInt($span.html());
+
 
 		if($pressed.attr("data-sign") == "-" && tempNum > 1){
 			tempNum--;
@@ -23,11 +27,40 @@ $(function(){
 
 	};
 
+//UNFINISHED    this will convert the time when it passes 0 seconds
 	var timeConvert = function(min, sec){
 		var tempMin;
 		var tempSec;
 	};
 
+
+//this will check the state of the clock and use the correct function
+	var timerStartCheck = function(){
+		if (timerOn == false){
+			;
+			timerOn = true;
+		}
+		else{
+			timerOn = false;
+		}
+	}
+
+//this is to start the functions of the clock
+	var clockFunctions = function(){
+		
+		var startTime = $("#timeDisplay").html();
+
+
+
+		timerStartCheck();
+
+
+	};
+
+
+//Events
 	$("button").on("click", settingChange);
 
+
+	$("#clockButton").on("click", clockFunctions);
 })
